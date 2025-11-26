c97c71ab7791d9e2907caf65f73201e6e9293967
Francisca Ramirez <107446606+franc-ramireza@users.noreply.github.com>
2025-11-26 17:48:59 -0300

    Avances

--- books2.py
+++ books2.py
@@ -1,12 +1,17 @@
+from typing import Optional
+
 from fastapi import FastAPI
+from pydantic import BaseModel , Field
+
 app = FastAPI()
 
 class Book:
     id : int
     title : str
     author : str
-    description : str
+    description: str
     rating : int
+
 
     def __init__(self,id,title,author,description,rating):
         self.id = id
@@ -14,6 +19,18 @@ class Book:
         self.author = author
         self.description = description
         self.rating = rating
+
+class BookRequest(BaseModel):
+    id: Optional[int] = None
+    title: str
+    author: str
+    description: str
+    rating: int
+    title: str = Field(min_length=3)
+    author: str = Field(min_length=1)
+    description: str = Field(min_length=1, max_length=100)
+    rating: int = Field(gt=0 , lt=6)
+
 
 BOOKS = [
         Book(1, 'Computer Science Pro', 'Pepito', 'Very nice book ', 5),
@@ -28,3 +45,20 @@ BOOKS = [
 @app.get("/books")
 async def read_all_books():
     return BOOKS
+
+#PostCrearlibro
+@app.post("/create_book")
+async def create_book(book_request : BookRequest):
+    new_book = Book(**book_request.model_dump())
+    BOOKS.append(find_book_id(new_book))
+
+def find_book_id(book:Book):
+    if len(BOOKS) >0:
+        book.id = BOOKS[-1].id+1
+
+    else:
+        book.id = 1
+    return book
+
+
+
